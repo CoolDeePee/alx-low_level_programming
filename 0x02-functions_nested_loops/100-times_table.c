af5ace74a9071254a4d9c0c73fce1b17869c7fd0
Dennis Olusehgun <famousodt17@gmail.com>
2023-06-21 09:09:51 -0700

    Function Nested Loop

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,7 +8,7 @@ void print_time_table(int n)
 {
 	int i, j, k;
 
-	if(n >= 0 && n <= 15)
+	if (n >= 0 && n <= 15)
 	{
 		for (i = 0; i <= n; i++)
 		{
@@ -31,7 +31,7 @@ void print_time_table(int n)
 				_putchar(' ');
 				_putchar(' ');
 				_putchar((k / 10) + '0');
-				_putchar((k % 10) + '0'); 
+				_putchar((k % 10) + '0');
 			} else if (k >= 100)
 			{
 				_putchar(',');
