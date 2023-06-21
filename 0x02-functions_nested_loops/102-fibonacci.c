abd9fb50e55ce0e48788b6f4d3db3b6fe47ca158
Dennis Olusehgun <famousodt17@gmail.com>
2023-06-21 11:17:50 -0700

    Times Table

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 
 /**
-* main - Prints first 50 Fibonacci numbers, starting with 1 and 2,
-*        separated by a comma followed by a space.
+* main - Prints first 50 Fibonacci numbers, starting with 1 and 2, separated by a comma followed by a space.
 *
 * Return: Always 0.
 */
@@ -24,5 +23,5 @@ int main(void)
 			printf(", ");
 	}
 
-			return (0);
+	return(0);
 }
